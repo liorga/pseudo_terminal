4efb63a6c38af996409073167aad8a3a45940d91
liorga <liorgal31892@gmail.com>
2021-01-18 16:34:48 +0200

    copy term start

---
+++ terminal_copy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int dbl_copy( int f1, int t1, int f2, int t2 );
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}

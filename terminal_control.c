a676b589111b8f82e7dc6d4b2362b3239125d325
liorga <liorgal31892@gmail.com>
2021-01-18 16:14:54 +0200

    update

--- terminal_control.c
+++ terminal_control.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <termio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int ctl_echo( int fd, int flag );
 int ctl_eof( int fd, int flag );
@@ -11,10 +12,27 @@ int ctl_eof( int fd, int flag );
 int main(int argc, char const *argv[])
 {
     int flag;
-    struct termios teminal;
-    int fd;
+    struct termios terminal;
+    int fd,res;
+    
+    if(tcgetattr(STDIN_FILENO,&terminal) < 0){
+        perror("tcgetattr2 failed");
+        return EXIT_FAILURE;
+    }
+
+    if (strcmp(argv[1],"echo on"))
+    {
+        flag = 1;
+    }else if (strcmp(argv[1],"echo off"))
+    {
+        flag = 0;
+    }
 
 
+    res = ctl_echo(STDIN_FILENO,flag);
+
+    //res = ctl_eof(STDIN_FILENO,flag);
+    
     
 
     return 0;
@@ -30,7 +48,7 @@ int ctl_echo( int fd, int flag ){
     else{
 
         if(tcgetattr(fd,&term) < 0){
-            perror("tcgetattr failed");
+            perror("tcgetattr3 failed");
             return EXIT_FAILURE;
         }
 
@@ -49,3 +67,34 @@ int ctl_echo( int fd, int flag ){
     
     return EXIT_SUCCESS;
 }
+
+int ctl_eof( int fd, int flag ){
+
+    struct termios term;
+
+    if (!isatty(fd)){
+        return EXIT_FAILURE;
+    }
+    else{
+
+        if(tcgetattr(fd,&term) < 0){
+            perror("tcgetattr4 failed");
+            return EXIT_FAILURE;
+        }
+
+        if (!flag){
+            term.c_cc[VEOF] = _POSIX_VDISABLE;
+        }
+        else{
+            term.c_cc[VEOF] = 4;
+        }
+        
+        if (tcsetattr(fd,TCSAFLUSH,&term) < 0){
+            perror("tcsetattr failed");
+            return EXIT_FAILURE;
+        }
+    }
+    
+    return EXIT_SUCCESS;
+}
+

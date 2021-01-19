8d585cb61c5067390318e01f79e0c8408c5db773
liorga <liorgal31892@gmail.com>
2021-01-19 12:30:28 +0200

    copy

--- terminal_copy.c
+++ terminal_copy.c
@@ -10,5 +10,30 @@ int dbl_copy( int f1, int t1, int f2, int t2 );
 int main(int argc, char const *argv[])
 {
     
+    pid_t pid;
+    int pipid[2];
+    int res;
+
+    if(pipe(pipid) == -1){
+        perror("pipe failed");
+        exit(EXIT_FAILURE);
+    }
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        return 2;
+    }else
+    {
+        //chiled proccess
+        
+    }
+    
+    
+
+
+
+
     return 0;
 }
